78c58c4ed4e8ccab8712983a93a8f2ee67fe5392
jolly-roger <igor.onyshchenko@gmail.com>
2016-09-21 10:44:55 +0300

    Avoid using rpython jit. Move imports to top

--- jspy.py
+++ jspy.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import os
-
-from rpython.rlib.objectmodel import enforceargs
-from rpython.rlib.parsing.parsing import ParseError
-from rpython.rlib.parsing.deterministic import LexerError
+import sys
 
 from js.exception import JsException
+from js.interpreter import Interpreter, load_file
 
 
 def main(argv):
@@ -14,18 +12,15 @@ def main(argv):
 
     try:
         run(files, opts)
-    except SystemExit:
-        printmessage(u"\n")
+    except:
+        print(u"\n")
 
     return 0
 
 
 def run(files, opts):
-    from js.interpreter import Interpreter, load_file
-
     interactive = len(files) == 0
     inspect = opts.get('inspect', False)
-
     interp = Interpreter(opts)
 
     for filename in files:
@@ -33,74 +28,51 @@ def run(files, opts):
 
         try:
             interp.run_src(src)
-        except ParseError as exc:
-            printsyntaxerror(unicode(filename), exc, src)
-            raise SystemExit
-        except LexerError as e:
-            printlexererror(unicode(filename), e, src)
-            raise SystemExit
         except JsException as e:
             printerrormessage(unicode(filename), e._msg())
-            raise SystemExit
+        except Exception as e:
+            printerror(unicode(filename), e, src)
 
     if inspect or interactive:
         repl(interp)
 
 
-@enforceargs(unicode, unicode)
 def printerrormessage(filename, message):
-    printmessage(u"ERROR in %s: %s\n" % (filename, message))
+    print(u"ERROR in %s: %s\n" % (filename, message))
 
 
 def repl(interpreter):
     filename = u'<stdin>'
     while True:
-        printmessage(u'js> ')
+        print(u'js> ')
         line = readline()
 
         try:
             result = interpreter.run_src(line)
             result_string = result.to_string()
-            printmessage(u"%s\n" % (result_string))
-        except ParseError as exc:
-            printsyntaxerror(filename, exc, line)
-            continue
-        except LexerError as e:
-            printlexererror(filename, e, line)
-            continue
+            print(u"%s\n" % (result_string))
         except JsException as e:
-            printerrormessage(filename, e._msg())
+            printerrormessage(unicode(filename), e._msg())
             continue
-
-
-# https://bitbucket.org/cfbolz/pyrolog/src/f18f2ccc23a4/prolog/interpreter/translatedmain.py
-@enforceargs(unicode)
-def printmessage(msg):
-    from js.runistr import encode_unicode_utf8
-    os.write(1, encode_unicode_utf8(msg))
+        except Exception as e:
+            printerror(unicode(filename), e, line)
+            continue
 
 
 def print_sourcepos(filename, source_pos, source):
     marker_indent = u' ' * source_pos.columnno
     error_lineno = source_pos.lineno - 1
     error_line = (source.splitlines())[error_lineno]
-    printmessage(u'Syntax Error in: %s:%d\n' % (unicode(filename), error_lineno))
-    printmessage(u'%s\n' % (unicode(error_line)))
-    printmessage(u'%s^\n' % (marker_indent))
+    print(u'Syntax Error in: %s:%d\n' % (unicode(filename), error_lineno))
+    print(u'%s\n' % (unicode(error_line)))
+    print(u'%s^\n' % (marker_indent))
 
 
-def printlexererror(filename, exc, source):
-    print_sourcepos(filename, exc.source_pos, source)
-
-
-def printsyntaxerror(filename, exc, source):
-    # XXX format syntax errors nicier
+def printerror(filename, exc, source):
     print_sourcepos(filename, exc.source_pos, source)
     error = exc.errorinformation.failure_reasons
-    printmessage(u'Error: %s\n' % (unicode(str(error))))
+    print(u'Error: %s\n' % (unicode(str(error))))
 
-
-# https://bitbucket.org/cfbolz/pyrolog/src/f18f2ccc23a4/prolog/interpreter/translatedmain.py
 def readline():
     result = []
     while True:
@@ -111,7 +83,7 @@ def readline():
         if s == '':
             if len(result) > 1:
                 break
-            raise SystemExit
+            raise Exception
     return "".join(result)
 
 
@@ -135,7 +107,6 @@ def parse_args(argv):
 
 
 if __name__ == '__main__':
-    import sys
     main(sys.argv)
 
 
@@ -143,11 +114,6 @@ if __name__ == '__main__':
 def target(driver, args):
     driver.exe_name = 'py-js'
     return entry_point, None
-
-
-def jitpolicy(driver):
-    from rpython.jit.codewriter.policy import JitPolicy
-    return JitPolicy()
 
 
 def entry_point(argv):
